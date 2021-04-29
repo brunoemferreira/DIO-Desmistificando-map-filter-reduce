ab7de3ddf482b0d739b496cd8854094d2ea5272c
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2021-04-29 04:55:31 -0300

    Create map_example.js

---
+++ Digital-innovation-one/map_example.js
@@ -0,0 +1,18 @@
+const pets [
+  {
+    type: '',
+    name: '',
+    age:  ,
+    weight:
+  }
+]
+
+
+
+/* 
+* Map 
+* Retornar um novo array com a mesma quantdiade de elementos
+* que o array inicial
+*/
+
+console.log(pets);
